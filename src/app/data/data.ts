a81ea0062c9e03c64453ad9093db508569165caf
bhaveshpadave <bhaveshpadave97@gmail.com>
2025-02-22 13:05:14 +0530

    Update data.ts to solve error

--- src/app/data/data.ts
+++ src/app/data/data.ts
@@ -1,25 +1,25 @@
 import { title } from "process";
 
-// export const blog = [
-//   {
-//     id: 1,
-//     title: 'Lorem Ipsum',
-//     content: `Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et
-//         consectetur ducimus vero placeat`,
-//   },
-//   {
-//     id: 2,
-//     title: 'Repellat Nihil',
-//     content: `Dolorem est fugiat occaecati voluptate velit esse. Dicta
-//     veritatis dolor quod et vel dire leno para dest`,
-//   },
-//   {
-//     id: 3,
-//     title: 'Ad ad velit qui',
-//     content: `Molestiae officiis omnis illo asperiores. Aut doloribus vitae
-//     sunt debitis quo vel nam quis`,
-//   },
-// ];
+export const blog = [
+  {
+    id: 1,
+    title: 'Lorem Ipsum',
+    content: `Ulamco laboris nisi ut aliquip ex ea commodo consequat. Et
+        consectetur ducimus vero placeat`,
+  },
+  {
+    id: 2,
+    title: 'Repellat Nihil',
+    content: `Dolorem est fugiat occaecati voluptate velit esse. Dicta
+    veritatis dolor quod et vel dire leno para dest`,
+  },
+  {
+    id: 3,
+    title: 'Ad ad velit qui',
+    content: `Molestiae officiis omnis illo asperiores. Aut doloribus vitae
+    sunt debitis quo vel nam quis`,
+  },
+];
 
 // export const events = [
 //   {
